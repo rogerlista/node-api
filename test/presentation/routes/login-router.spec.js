d7e256b69284ed44afafed38e83764daaf5d7521
Rogério Gomes Rios <roger.lista@gmail.com>
2021-09-04 06:20:48 -0300

    feat(LoginRouter): garantirá o retorno da exceção ServerError se o método auth lançar uma exceção

--- test/presentation/routes/login-router.spec.js
+++ test/presentation/routes/login-router.spec.js
@@ -152,5 +152,6 @@ describe('Login Router', () => {
     const httpResponse = await sut.route(httpRequest)
 
     expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 })
