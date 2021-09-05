5601eb23384c967705bcd4f70b88108aee9acc42
Rogério Gomes Rios <roger.lista@gmail.com>
2021-09-05 13:23:13 -0300

    refactor(AuthUseCase): garantirá que o método auth receba um objeto com as propriedades corretas

--- test/domain/use-case/auth-use-case.spec.js
+++ test/domain/use-case/auth-use-case.spec.js
@@ -1,7 +1,7 @@
 const { ParametroObrigatorioError } = require('../../../src/lib/error')
 
 class AuthUseCase {
-  async auth(email, senha) {
+  async auth({ email, senha }) {
     if (!email) {
       throw new ParametroObrigatorioError('E-mail')
     }
@@ -20,7 +20,7 @@ describe('Auth Use Case', () => {
   test('deve lançar uma exceção se o e-mail não for informado', async () => {
     const sut = makeSut()
 
-    const promise = sut.auth()
+    const promise = sut.auth({})
 
     await expect(promise).rejects.toThrow(
       new ParametroObrigatorioError('E-mail')
@@ -30,7 +30,7 @@ describe('Auth Use Case', () => {
   test('deve lançar uma exceção se a senha não for informada', async () => {
     const sut = makeSut()
 
-    const promise = sut.auth('email_valido@mail.com')
+    const promise = sut.auth({ email: 'email_valido@mail.com' })
 
     await expect(promise).rejects.toThrow(
       new ParametroObrigatorioError('Senha')
