56c90143c906cf61dc86c10be55926761a65ec62
Rogério Gomes Rios <roger.lista@gmail.com>
2021-09-05 13:14:48 -0300

    feat(AuthUseCase): garantirá que uma exceção seja lançada se a senha não for informada

--- test/domain/use-case/auth-use-case.spec.js
+++ test/domain/use-case/auth-use-case.spec.js
@@ -1,9 +1,13 @@
 const { ParametroObrigatorioError } = require('../../../src/lib/error')
 
 class AuthUseCase {
-  async auth(email) {
+  async auth(email, senha) {
     if (!email) {
       throw new ParametroObrigatorioError('E-mail')
+    }
+
+    if (!senha) {
+      throw new ParametroObrigatorioError('Senha')
     }
   }
 }
@@ -16,6 +20,16 @@ describe('Auth Use Case', () => {
 
     await expect(promise).rejects.toThrow(
       new ParametroObrigatorioError('E-mail')
+    )
+  })
+
+  test('deve lançar uma exceção se a senha não for informada', async () => {
+    const sut = new AuthUseCase()
+
+    const promise = sut.auth('email_valido@mail.com')
+
+    await expect(promise).rejects.toThrow(
+      new ParametroObrigatorioError('Senha')
     )
   })
 })
