a3586ac1f303960ff56c5e43532e1ca8f19c5e68
Rogério Gomes Rios <roger.lista@gmail.com>
2021-09-06 04:55:57 -0300

    feat(AuthUseCase): garantirá que FinUserByEmailRepositorio seja chamado com um email correto

--- test/domain/use-case/auth-use-case.spec.js
+++ test/domain/use-case/auth-use-case.spec.js
@@ -1,6 +1,10 @@
 const { ParametroObrigatorioError } = require('../../../src/lib/error')
 
 class AuthUseCase {
+  constructor(findUserByEmailRepository) {
+    this.findUserByEmailRepository = findUserByEmailRepository
+  }
+
   async auth({ email, senha }) {
     if (!email) {
       throw new ParametroObrigatorioError('E-mail')
@@ -9,16 +13,30 @@ class AuthUseCase {
     if (!senha) {
       throw new ParametroObrigatorioError('Senha')
     }
+
+    await this.findUserByEmailRepository.find(email)
   }
 }
 
 const makeSut = () => {
-  return new AuthUseCase()
+  class FindUserByEmailRepository {
+    find(email) {
+      this.email = email
+    }
+  }
+
+  const findUserByEmailRepository = new FindUserByEmailRepository()
+  const sut = new AuthUseCase(findUserByEmailRepository)
+
+  return {
+    sut,
+    findUserByEmailRepository,
+  }
 }
 
 describe('Auth Use Case', () => {
   test('deve lançar uma exceção se o e-mail não for informado', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const promise = sut.auth({})
 
@@ -28,12 +46,20 @@ describe('Auth Use Case', () => {
   })
 
   test('deve lançar uma exceção se a senha não for informada', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
 
     const promise = sut.auth({ email: 'email_valido@mail.com' })
 
     await expect(promise).rejects.toThrow(
       new ParametroObrigatorioError('Senha')
     )
+  })
+
+  test('deve chamar FindUserByEmailRepository com um e-mail correto', async () => {
+    const { sut, findUserByEmailRepository } = makeSut()
+
+    await sut.auth({ email: 'email_valido@mail.com', senha: 'senha_valida' })
+
+    expect(findUserByEmailRepository.email).toBe('email_valido@mail.com')
   })
 })
