8770fd4a50e461f1e92d812bb602e2ae1baec95b
Rogério Gomes Rios <roger.lista@gmail.com>
2021-09-11 11:55:40 -0300

    feat(Express): garantirá que um xml content-type seja retornado se forçado

--- test/main/middleware/content-type.test.js
+++ test/main/middleware/content-type.test.js
@@ -11,4 +11,14 @@ describe('ContentType', () => {
     expect(res.headers['content-type']).toBe('application/json; charset=utf-8')
     expect(res.headers['content-type']).toMatch(/json/)
   })
+
+  test('deve retornar um xml content-type se forçado', async () => {
+    app.get('/content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    const res = await request(app).get('/content_type_xml')
+    expect(res.headers['content-type']).toBe('application/xml; charset=utf-8')
+    expect(res.headers['content-type']).toMatch(/xml/)
+  })
 })
