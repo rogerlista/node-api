d4abfeb676363c2d2d3109dd93fea7ec497fae9c
Rogério Gomes Rios <roger.lista@gmail.com>
2021-09-11 11:30:39 -0300

    refactor(app): moverá middlewares para o arquivo setup

--- src/main/app.js
+++ src/main/app.js
@@ -1,9 +1,7 @@
 const express = require('express')
 
 const { setup } = require('./config')
-const { cors, jsonParse } = require('./middleware')
+
 const app = express()
 setup(app)
-app.use(cors)
-app.use(jsonParse)
 module.exports = app
--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,3 +1,7 @@
+const { cors, jsonParse } = require('../middleware')
+
 module.exports = (app) => {
   app.disable('x-powered-by')
+  app.use(cors)
+  app.use(jsonParse)
 }
