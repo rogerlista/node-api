89dc4e39fce2b5673f1477cba663f58563a259bd
Rogério Gomes Rios <roger.lista@gmail.com>
2021-09-11 12:32:43 -0300

    refactor(content-type): garantirá que json e o padrão

--- test/main/middleware/content-type.test.js
+++ test/main/middleware/content-type.test.js
@@ -5,7 +5,7 @@ const app = require('../../../src/main/app')
 describe('ContentType', () => {
   test('deve retornar um json content-type como default', async () => {
     app.get('/content_type_json', (req, res) => {
-      res.send({})
+      res.send('')
     })
     const res = await request(app).get('/content_type_json')
     expect(res.headers['content-type']).toBe('application/json; charset=utf-8')
