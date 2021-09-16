fb5735bee9d3757e61d440c493d8197844932bff
Rogério Gomes Rios <roger.lista@gmail.com>
2021-09-16 20:11:04 -0300

    test(middleware): reset jest modules in content-type

--- test/main/middleware/content-type.test.js
+++ test/main/middleware/content-type.test.js
@@ -1,23 +1,28 @@
 const request = require('supertest')
 
-const app = require('../../../src/main/app')
-
 describe('ContentType', () => {
+  let app
+
+  beforeEach(() => {
+    jest.resetModules()
+    app = require('../../../src/main/app')
+  })
+
   test('deve retornar um json content-type como default', async () => {
-    app.get('/content_type_json', (req, res) => {
+    app.get('/content_type', (req, res) => {
       res.send('')
     })
-    const res = await request(app).get('/content_type_json')
+    const res = await request(app).get('/content_type')
     expect(res.headers['content-type']).toBe('application/json; charset=utf-8')
     expect(res.headers['content-type']).toMatch(/json/)
   })
 
   test('deve retornar um xml content-type se forçado', async () => {
-    app.get('/content_type_xml', (req, res) => {
+    app.get('/content_type', (req, res) => {
       res.type('xml')
       res.send('')
     })
-    const res = await request(app).get('/content_type_xml')
+    const res = await request(app).get('/content_type')
     expect(res.headers['content-type']).toBe('application/xml; charset=utf-8')
     expect(res.headers['content-type']).toMatch(/xml/)
   })
